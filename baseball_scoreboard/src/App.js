3e1d9cb16810a5652e332de6992f5f19b7d9e8ad
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-16 15:19:36 -0600

    added in state

--- baseball_scoreboard/src/App.js
+++ baseball_scoreboard/src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 
 function App() 
 {
+  const [strikes, setStrikes] = useState(0);
+  const [balls, setBalls] = useState(0);
+
   return (
     <div className="App">
       <h1>Baseball Scoreboard</h1>
      <section className='scoreboard'>
         <div className='strikes'>
           <h3 className='strike-title'>Strikes</h3>
-          <div className='strike-value'>0</div>
+          <div className='strike-value'>{strikes}</div>
         </div>
         <div className='balls'>
           <h3 className='ball-title'>Balls</h3>
-          <div className='ball-value'>0</div>
+          <div className='ball-value'>{balls}</div>
         </div>
      </section>
     </div>
