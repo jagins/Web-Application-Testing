0888e1cdacefb1652276723bba89fb0d8764983a
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-16 15:14:42 -0600

    added scoreboard skeleton and useState hook

--- baseball_scoreboard/src/App.js
+++ baseball_scoreboard/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() 
 {
   return (
     <div className="App">
-     
+      <h1>Baseball Scoreboard</h1>
+     <div className='scoreboard'>
+        <div className='strikes'>
+          <h3 className='strike-title'>Strikes</h3>
+          <div className='strike-value'>0</div>
+        </div>
+        <div className='balls'>
+          <h3 className='ball-title'>Balls</h3>
+          <div className='ball-value'>0</div>
+        </div>
+     </div>
     </div>
   );
 }
