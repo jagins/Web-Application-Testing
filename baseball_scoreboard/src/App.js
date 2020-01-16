2846fdec08190e50c7c7c740e20d072493a93012
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-16 16:03:59 -0600

    Changed reset button to hit button does the same thing resets count to 0

--- baseball_scoreboard/src/App.js
+++ baseball_scoreboard/src/App.js
@@ -31,7 +31,7 @@ function App()
           <button onClick={() => setStrikes(addStrikes(strikes, false))}>Add Strike</button>
           <button onClick={() => setBalls(addBalls)}>Add Ball</button>
           <button onClick={() => setStrikes(addStrikes(strikes, true))}>Foul Ball</button>
-          <button onClick={reset}>Reset</button>
+          <button onClick={() => {setStrikes(0); setBalls(0);}}>Hit</button>
         </div>
       </section>
      </section>
